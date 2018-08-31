81e0a9c4e3a0029a0519573e09250ff980b9235f
cflux1990 <cflux.maltez@live.com>
2018-08-30 20:23:13 -0700

    3.36 - Working with props

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App"> {/* next everything in one root element*/}
         <h1>Hi, i'm a react app</h1>
-      <Person /> {/*This, along with the Person import brings in the Person component */}
-      <Person />
-      <Person />
+      <Person name="Max" age="28"/> {/*This, along with the Person import brings in the Person component */}
+      <Person name="Manu" age="28"/>
+      <Person name="Stephanie" age="26"/>
       </div>
 
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,20 @@
 import React from 'react';
 
 
-const person = () => { // research arrow functions
-    return <p> i'm a person and i am {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => { // research arrow functions. using props means you bring in the properties of the component
+    return <p> i'm a {props.name} and i am {props.age} years old!</p>
     {/*to use js stuff inline, wrap in curly braces, can only do simple expressions for now*/}
+
+
 }
 
 export default person;
+
+
+// When using class-based components, its this.props
+
+// class Person extends Component {
+//     render () {
+//         return <p>My name is {this.props}</p>
+//     }
+// }
