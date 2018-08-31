208fe0432f4bb221a019e6ed6103a33cabb6edb4
cflux1990 <cflux.maltez@live.com>
2018-08-30 20:29:07 -0700

    3.37 - Understanding the Children Property

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App"> {/* next everything in one root element*/}
         <h1>Hi, i'm a react app</h1>
       <Person name="Max" age="28"/> {/*This, along with the Person import brings in the Person component */}
-      <Person name="Manu" age="28"/>
+          <Person name="Manu" age="28"> My hobbies: Racing</Person>
       <Person name="Stephanie" age="26"/>
       </div>
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,9 +4,14 @@ import React from 'react';
 
 
 const person = (props) => { // research arrow functions. using props means you bring in the properties of the component
-    return <p> i'm a {props.name} and i am {props.age} years old!</p>
-    {/*to use js stuff inline, wrap in curly braces, can only do simple expressions for now*/}
+    return (
 
+        <div>
+            <p> i'm a {props.name} and i am {props.age} years old!</p>
+    {/*to use js stuff inline, wrap in curly braces, can only do simple expressions for now*/}
+            <p>{props.children}</p> {/* children refers to any elements between the opening and closing tags of component*/}
+        </div>
+)
 
 }
 
