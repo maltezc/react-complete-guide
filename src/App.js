59dd3e97b856184f0f1ee00660443a7ad58760d7
cflux1990 <cflux.maltez@live.com>
2018-08-30 20:38:11 -0700

    3.38 - Understanding & Using State

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person'; //first letter is capitalized so it identifies as a custom component
 
 class App extends Component {
+    state = { // use state carefully
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephanie', age: 26 }
+        ]
+    }
   render() {
     return (
       <div className="App"> {/* next everything in one root element*/}
         <h1>Hi, i'm a react app</h1>
-      <Person name="Max" age="28"/> {/*This, along with the Person import brings in the Person component */}
-          <Person name="Manu" age="28"> My hobbies: Racing</Person>
-      <Person name="Stephanie" age="26"/>
+          <p>This is working!</p>
+          <button>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> {/*This, along with the Person import brings in the Person component */}
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Racing</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
 
     );
